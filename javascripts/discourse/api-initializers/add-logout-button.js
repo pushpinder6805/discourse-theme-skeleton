0a317ec6a34d78402e77256ad953d9e4aac0426a
pinder99 <pinder.chaudhary@gmail.com>
2024-12-25 14:25:31 +0530

    update

---
+++ javascripts/discourse/api-initializers/add-logout-button.js
@@ -0,0 +1,7 @@
+import LogoutHeaderButton from "../components/logout-header-button";
+
+export default apiInitializer("1.28.0", (api) => {
+  if (api.getCurrentUser()) {
+    api.headerIcons.add("logout", LogoutHeaderButton, { before: "auth" });
+  }
+});
