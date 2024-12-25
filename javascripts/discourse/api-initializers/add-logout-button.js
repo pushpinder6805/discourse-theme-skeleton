8ddca41d9b6ebcbb472f89a2622da8aa8012a45a
pinder99 <pinder.chaudhary@gmail.com>
2024-12-25 14:28:20 +0530

    Update add-logout-button.js

--- javascripts/discourse/api-initializers/add-logout-button.js
+++ javascripts/discourse/api-initializers/add-logout-button.js
@@ -1,3 +1,4 @@
+import { apiInitializer } from "discourse-common/lib/api-initializers";
 import LogoutHeaderButton from "../components/logout-header-button";
 
 export default apiInitializer("1.28.0", (api) => {
